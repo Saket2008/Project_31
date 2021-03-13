1fbc5ba33ada107b24ccdb0e3b49a058d43586d9
Saket2008 <74353880+Saket2008@users.noreply.github.com>
2021-03-13 21:02:16 +0530

    Add files via upload

---
+++ plinko.js
@@ -0,0 +1,18 @@
+class Plinko{
+    constructor(x, y, radius, color){
+        var options = {
+            isStatic: true
+        }
+        this.body = Bodies.circle(x, y, radius, options);
+        this.radius = radius;
+        this.color = color;
+        World.add(world, this.body);
+    }
+
+    display(){
+        var pos = this.body.position;
+        fill(this.color);
+        ellipseMode(RADIUS);
+        ellipse(pos.x, pos.y, this.radius, this.radius);
+    }
+}
